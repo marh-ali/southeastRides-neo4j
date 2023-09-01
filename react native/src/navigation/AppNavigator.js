4601b79b2d58fc7885ba71deabc49441a5a5b49c
marh-ali <aam261@Georgetown.edu>
2023-09-01 12:10:28 -0400

    refactor frontend and backend

--- react native/src/navigation/AppNavigator.js
+++ react native/src/navigation/AppNavigator.js
@@ -1,9 +1,7 @@
-// In App.js in a new project
-
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text } from "react-native";
 
 function HomeScreen() {
   return (
@@ -21,15 +19,13 @@ function DetailsScreen() {
 }
 const Stack = createNativeStackNavigator();
 
-function App() {
+function AppNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Details">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
   );
 }
 
-export default App;
+export default AppNavigator;
